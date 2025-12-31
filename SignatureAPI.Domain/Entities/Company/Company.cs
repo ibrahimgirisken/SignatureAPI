e0512d87f4297bc6972b8f3d5d2b76153d45691a
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2025-12-31 11:10:53 +0300

    Add project files.

---
+++ SignatureAPI.Domain/Entities/Company/Company.cs
@@ -0,0 +1,14 @@
+﻿using SignatureAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignatureAPI.Domain.Entities.Company
+{
+    public class Company:BaseEntity
+    {
+        public string CompanyName { get; set; }
+    }
+}
---
+++ SignatureAPI.Domain/Entities/Signature/Signature.cs
@@ -0,0 +1,20 @@
+﻿using SignatureAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignatureAPI.Domain.Entities.Signature
+{
+    public class Signature:BaseEntity
+    {
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Address { get; set; }
+        public string KdvText{ get; set; }
+        public string InformationText { get; set; }
+        public string EnvironmentText { get; set; }
+        public Guid? CompanyId { get; set; }
+    }
+}
